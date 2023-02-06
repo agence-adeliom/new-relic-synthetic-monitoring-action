60e29f71d9e59ef4208c0118ba91ce6db8654b90
Jean-David Daviet <jean-david.daviet@adeliom.com>
2023-02-06 14:10:46 +0100

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const created: string = core.getInput('create');
+    const updated: string = core.getInput('update');
+    const deleted: string = core.getInput('delete');
+    core.setOutput('create', created);
+    core.setOutput('update', updated);
+    core.setOutput('delete', deleted);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
