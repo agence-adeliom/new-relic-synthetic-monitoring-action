9b27e0ae5b250611a997401ca237f853cadefc8d
Jean-David Daviet <jean-david.daviet@adeliom.com>
2023-02-06 14:24:04 +0100

    add content

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ async function run(): Promise<void> {
     const created: string = core.getInput('create');
     const updated: string = core.getInput('update');
     const deleted: string = core.getInput('delete');
+    const content: string = core.getInput('content');
     core.setOutput('create', created);
     core.setOutput('update', updated);
     core.setOutput('delete', deleted);
+    core.setOutput('content', content);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
